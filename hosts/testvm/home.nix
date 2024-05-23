51924e9d6b003e6877f9e0b79f7c36e67e8a3b32
fredrikscode <fredrik@kihlstedt.io>
2024-05-23 23:22:38 +0200

    First upload

---
+++ hosts/testvm/home.nix
@@ -0,0 +1,118 @@
+{
+  config,
+  pkgs,
+  inputs,
+  username,
+  host,
+  gtkThemeFromScheme,
+  ...
+}:
+let
+  palette = config.colorScheme.palette;
+  inherit (import ./vars.nix)
+    gitUsername
+    gitEmail
+    theme
+    ;
+in
+{
+  # Home Manager Settings
+  home.username = "${username}";
+  home.homeDirectory = "/home/${username}";
+  home.stateVersion = "23.11";
+
+  # Set The Colorscheme
+  colorScheme = inputs.nix-colors.colorSchemes."${theme}";
+
+  # Import Program Configurations
+  imports = [
+    inputs.nix-colors.homeManagerModules.default
+    inputs.hyprland.homeManagerModules.default
+    ../../config/hyprland.nix
+    ../../config/swaync.nix
+    ../../config/waybar.nix
+    ../../config/wlogout.nix
+  ];
+
+  # Place Files Inside Home Directory
+  home.file."Pictures/Wallpapers" = {
+    source = ../../config/wallpapers;
+    recursive = true;
+  };
+
+  home.file.".config/wlogout/icons" = {
+    source = ../../config/wlogout;
+    recursive = true;
+  };
+
+  home.file.".local/share/fonts" = {
+    source = ../../config/fonts;
+    recursive = true;
+  };
+
+  home.file.".config/starship.toml".source = ../../config/starship.toml;
+  home.file.".base16-themes".source = ../../config/base16-themes;
+  home.file.".emoji".source = ../../config/emoji;
+
+  # Install & Configure Git
+  programs.git = {
+    enable = true;
+    userName = "${gitUsername}";
+    userEmail = "${gitEmail}";
+  };
+
+  # Create XDG Dirs
+  xdg = {
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
+  };
+
+  # Configure Cursor Theme
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+  };
+
+  # Theme GTK
+  gtk = {
+    enable = true;
+    font = {
+      name = "Ubuntu";
+      size = 12;
+      package = pkgs.ubuntu_font_family;
+    };
+    theme = {
+      name = "${config.colorScheme.slug}";
+      package = gtkThemeFromScheme { scheme = config.colorScheme; };
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+
+  # Theme QT -> GTK
+  qt = {
+    enable = true;
+    platformTheme = "gtk";
+    style = {
+      name = "adwaita-dark";
+      package = pkgs.adwaita-qt;
+    };
+  };
+
+  programs = {
+    home-manager.enable = true;
+  };
+}
